cb6dd8c75122d7043605448c5f569c2f8c454484
Meidon <Meidon@users.noreply.github.com>
2018-03-09 13:49:51 +0100

    The Initial repository with AIML 2.0

---
+++ PERSUADE/Assets/AIML 2.0/Scripts/InitializeDamarion.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+
+public class InitializeDamarion : MonoBehaviour
+{
+    private ChatbotDamarion bot;
+    public InputField inputField;
+    public Text robotOutput;
+
+    void Start()
+    {
+        bot = new ChatbotDamarion();
+        bot.LoadBrain();
+    }
+
+
+    void OnDisable()
+    {
+        bot.SaveBrain();
+    }
+
+
+    public void SendQuestionToRobot()
+    {
+        if (string.IsNullOrEmpty(inputField.text) == false)
+        {
+            var answer = bot.getOutput(inputField.text);
+            robotOutput.text = answer;
+            inputField.text = string.Empty;
+        }
+    }
+
+
+}
---
+++ PERSUADE/Assets/AIML 2.0/Scripts/Narrator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Narrator : MonoBehaviour {
+
+    public Text exampleText;
+    public Text robotOutput;
+    private string keyword1;
+    private string keyword2;
+    private string output;
+
+	// Use this for initialization
+	void Start () {
+        keyword1 = "Name: Damarion";
+        keyword2 = "Girlfriend: Juliet";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(robotOutput.text.Contains("Damarion"))
+        {
+            output = keyword1;
+            UpdateText();
+        }
+
+        if (robotOutput.text.Contains("Juliet"))
+        {
+            if(output.Contains("Damarion"))
+            {
+                output = keyword1+"\n"+keyword2;
+                UpdateText();
+            } else
+            {
+                output = keyword2;
+                UpdateText();
+            }
+            
+        }
+    }
+
+    private void UpdateText()
+    {
+        exampleText.text = output;
+    }
+}
