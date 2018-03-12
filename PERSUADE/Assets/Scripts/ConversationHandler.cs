30cf37e198358df47e7ae41263976f539a6e4175
Meidon <Meidon@users.noreply.github.com>
2018-03-12 15:58:50 +0100

    New Additions to the project
    -Invector's 3rdPerson Controller
    - ProCore (Probuilder+ProGrids)
    - Fixes and introduces new Bugs
    - ConversationHandler

---
+++ PERSUADE/Assets/Scripts/ConversationHandler.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConversationHandler : MonoBehaviour {
+
+    public bool isRoaming;
+    public GameObject ChatbotUID;
+    public Camera PlayerCam;
+    public GameObject PlayerController;
+    public bool isUIEnabled;
+    public Canvas tempC;
+
+	void Start () {
+        isRoaming = true;
+        isUIEnabled = false;
+        PlayerCam = Camera.main;
+        PlayerController = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	void Update () {
+		
+        if(isRoaming)
+        {
+            isUIEnabled = false;
+            PlayerController.GetComponent<Invector.CharacterController.vThirdPersonInput>().enabled = true;
+        }
+        if(isUIEnabled)
+        {
+            PlayerController.GetComponent<Invector.CharacterController.vThirdPersonInput>().enabled = false;
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isRoaming = true;
+            if(tempC != null)
+                tempC.enabled = false;
+        }
+
+        Ray r = PlayerCam.ScreenPointToRay(Vector3.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(r, out hit, 20))
+        {
+            if(hit.transform.gameObject.tag == "Chatbot")
+            {
+                tempC = hit.transform.gameObject.GetComponentInChildren<Canvas>();
+            }
+
+            if(tempC != null)
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    isRoaming = false;
+                    isUIEnabled = true;
+                    tempC.enabled = true;
+                }
+            }
+
+        }
+    }
+}
