aac46759c189ae2b8e0020608d6337e58092b00e
Meidon <Meidon@users.noreply.github.com>
2018-03-23 16:57:45 +0100

    script setup for testing scene

--- PERSUADE/Assets/Scripts/Narrator.cs
+++ PERSUADE/Assets/Scripts/Narrator.cs
@@ -8,6 +8,7 @@ public class Narrator : MonoBehaviour {
     public string textInput;
     public List<string> DataLog = new List<string>();
     public List<string> Keywords = new List<string>();
+    
 	
 	void Update () {
         for(int i = 0; i <= Keywords.Count-1; i++)
