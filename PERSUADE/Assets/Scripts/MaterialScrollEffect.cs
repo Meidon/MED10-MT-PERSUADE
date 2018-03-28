fd34cd3642fee46ffbbb3a93d2d3c649244282b2
Meidon <Meidon@users.noreply.github.com>
2018-03-28 14:28:30 +0200

    More level design
    -new script for material offset
    - more level design, probably redoing stuff as i am not satisfied with how it looks atm.

---
+++ PERSUADE/Assets/Scripts/MaterialScrollEffect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialScrollEffect : MonoBehaviour {
+    public float offsetSpeed = 0.5f;
+    private Renderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update () {
+        float yOffset = Time.time * offsetSpeed;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(0,yOffset));
+
+        if(yOffset >= 20)
+        {
+            yOffset = 0;
+        }
+	}
+}
